8d16ce08c37aba779ed6ad3ad1ecdd1b81d1f58d
Anmoldeep <anmoldeepsingh.lnu1@rdpolytech.ca>
2025-05-13 13:53:07 -0600

    initial commit

---
+++ python_examples.py
@@ -0,0 +1,48 @@
+#this is a connect 
+x = 5 
+print(x)
+
+'''
+
+this is  an example of a tuple.
+Tuples are ordered arrays that cannot be changed.
+'''
+mytuple = ('honda', 'ford', 'polo')
+
+
+#lists (arrays)
+list = ['apple', 'banana', 'cherry']
+
+#data dictionaries. key-value or objects like 
+dictionary = {'Anmol' "20", 'Amit' "25", 'Amit' "30"}
+
+#if-else conditional logic 
+a = 33
+b = 220
+
+if a > b:
+    print ('a is greater than ')
+elif a < b:
+    print("b is greater than a")
+else:
+    print("a is equal to b")
+
+#looping in python
+colours = ['red', 'blue', 'green','blue']
+
+
+#for-in loop 
+for x in colours:
+    print(x)
+
+    #while loop
+i = 1
+while i < 10:
+    print(i)
+    i += 1
+
+#functions
+def add(a,b):
+    return a + b
+
+print(add(14, 5))
